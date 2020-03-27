4adb046c846cd4d77f1bf8e977361b80cc449c46
mahsa <farahanimahsa99@gmail.com>
2020-03-27 16:42:07 -0700

    add seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(NotesTableSeeder::class);
+        $this->call(ProjectsTableSeeder::class);
+        $this->call(TasksTableSeeder::class);
+        $this->call(TeamsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
