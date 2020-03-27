8932b38b79b52a9dd0361389dfc395119164d378
mahsa <farahanimahsa99@gmail.com>
2020-03-27 16:29:19 -0700

    change migrations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,17 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'user_id' => User::all()->random()->user_id,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'job' => $faker->jobTitle,
+        'permission' => $faker->randomKey(['user', 'admin']),
+        'bio' => $faker->text(70),
+        'name' => $faker->image(),
     ];
 });
+
