50bfa48bce536e929f53ebf0c4afb6af83ffaa07
Mahsa Farahani <38652406+mahsafrhni@users.noreply.github.com>
2020-04-02 12:07:03 -0700

    just change some http methods"post" to put & delete

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::middleware(['auth', 'throttle:30,1'])->group(function () {
     //users-just for admin
     Route::prefix('users')->name('users.')->group(function () {
         Route::get('/', 'UsersController@index')->name('index');
-        Route::post('/edit', 'UsersController@edit')->name('edit');
-        Route::post('/delete', 'UsersController@delete')->name('delete');
+        Route::put('/edit', 'UsersController@edit')->name('edit');
+        Route::delete('/delete', 'UsersController@delete')->name('delete');
         // and also other users can access the other user's information through this route
         Route::get('/{user}', 'UsersController@show')->name('show');
     });
@@ -31,28 +31,28 @@ Route::middleware(['auth', 'throttle:30,1'])->group(function () {
         Route::get('/', 'ProjectsController@index')->name('index');
         Route::post('/add', 'ProjectsController@add')->name('add');
         Route::get('/{id}', 'ProjectsController@show($user)')->name('show');
-        Route::post('/update', 'ProjectsController@update')->name('update');
-        Route::post('/delete', 'ProjectsController@delete')->name('delete');
+        Route::put('/update', 'ProjectsController@update')->name('update');
+        Route::delete('/delete', 'ProjectsController@delete')->name('delete');
         Route::get('/edit', 'ProjectsController@edit')->name('edit');
     });
     Route::prefix('team')->name('team.')->group(function () {
         Route::post('/insert', 'TeamController@insert')->name('insert');
-        Route::post('/delete', 'TeamController@delete')->name('delete');
-        Route::post('/update', 'TeamController@update')->name('update');
+        Route::delete('/delete', 'TeamController@delete')->name('delete');
+        Route::put('/update', 'TeamController@update')->name('update');
         Route::get('/{id}/edit', 'TeamController@edit')->name('edit');
     });
     Route::prefix('task')->name('task.')->group(function () {
         Route::post('/add', 'TasksController@add')->name('add');
-        Route::post('/update', 'TasksController@update')->name('update');
-        Route::post('/delete', 'TasksController@delete')->name('delete');
+        Route::put('/update', 'TasksController@update')->name('update');
+        Route::delete('/delete', 'TasksController@delete')->name('delete');
     });
     Route::prefix('note')->name('note.')->group(function () {
         Route::post('/add', 'NotesController@add')->name('add');
-        Route::post('/update', 'NotesController@update')->name('update');
-        Route::post('/delete', 'NotesController@delete')->name('delete');
+        Route::put('/update', 'NotesController@update')->name('update');
+        Route::delete('/delete', 'NotesController@delete')->name('delete');
     });
     Route::prefix('account')->name('account.')->group(function () {
         Route::get('/', 'AccountController@index')->name('edit');
-        Route::post('/', 'AccountController@update')->name('update');
+        Route::put('/', 'AccountController@update')->name('update');
     });
 });
